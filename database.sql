ceb28cb413c6f5d47a02e41724e026eb0ce2d730
Hirotoshi Uchida <uho02741358@gmail.com>
2025-11-01 12:29:49 +0900

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT);
+INSERT INTO users (name, email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO users (name, email) VALUES ('Bob', 'bob@example.com');
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+  content: ["./After/HTMX/**/*.html", "./App.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
