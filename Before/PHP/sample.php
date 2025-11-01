60198194fa8ec6d76278565b9c12f33f967354ed
Hirotoshi Uchida <uho02741358@gmail.com>
2025-11-01 12:31:03 +0900

    Create sample.php

---
+++ Before/PHP/sample.php
@@ -0,0 +1,18 @@
+<?php echo "
+<h2>🧠 Technologies Used</h2>
+<br>
+<br>
+<h3><li>PHP / Laravel / Hack / TypeScript 
+<br>
+↓
+<br>converted into JavaScript via `phptojs` and `tsc`.</li></h3>
+<br>
+<h3><li>Scss / Sass / Less
+<br>
+↓
+<br>compiled via Tailwind CSS.</li></h3>
+<br>
+<h3><li>HTMX + Hyperscript for reactive HTML interface.</li></h3>
+<h3><li>Vite for bundling.</li></h3>
+<h3><li>GitHub Actions + Pages</li></h3>
+<br>
